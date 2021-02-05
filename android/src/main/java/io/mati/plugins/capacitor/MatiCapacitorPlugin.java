c3a90ea5368bea01e5faa6b2a5b56915927a4e9c
andrey.posnov <andrey.posnov@mati.io>
2021-02-05 19:23:21 +0300

    alpha version available

--- android/src/main/java/io/mati/plugins/capacitor/MatiCapacitorPlugin.java
+++ android/src/main/java/io/mati/plugins/capacitor/MatiCapacitorPlugin.java
@@ -1,5 +1,13 @@
 package io.mati.plugins.capacitor;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.FrameLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.getcapacitor.JSObject;
 import com.getcapacitor.NativePlugin;
 import com.getcapacitor.Plugin;
@@ -16,10 +24,13 @@ import com.matilock.mati_kyc_sdk.Metadata;
 
 import org.jetbrains.annotations.Nullable;
 
+import java.lang.ref.WeakReference;
+
 @NativePlugin
 public class MatiCapacitorPlugin extends Plugin implements MatiCallback {
 
     private MatiCallbackManager mCallbackManager = MatiCallbackManager.createNew();
+    public static String SPEC_FLOW_ID = "";
 
     @PluginMethod
     public void initialization(PluginCall call) {
@@ -34,23 +45,15 @@ public class MatiCapacitorPlugin extends Plugin implements MatiCallback {
 
     @PluginMethod
     public void setParams(PluginCall call) {
-
-        Mati.init(getContext(), call.getString("clientId"));
-
-        MatiLoginManager.getInstance().registerCallback(mCallbackManager, this);
-        MatiLoginManager.getInstance().registerCallback(mCallbackManager, Mati);
-
+        SPEC_FLOW_ID = call.getString("flowId");
         call.success();
     }
 
     @PluginMethod
     public void showFlow(PluginCall call) {
-
-        Mati.init(getContext(), call.getString("clientId"));
-
-        MatiLoginManager.getInstance().registerCallback(mCallbackManager, this);
-        MatiLoginManager.getInstance().registerCallback(mCallbackManager, Mati);
-
+        MatiLoginButtonLauncher.weakReferenceCallbackManager = new WeakReference<>(mCallbackManager);
+        Intent intent = new Intent(getActivity(), MatiCapacitorPlugin.MatiLoginButtonLauncher.class);
+        getActivity().startActivity(intent);
         call.success();
     }
 
@@ -67,6 +70,52 @@ public class MatiCapacitorPlugin extends Plugin implements MatiCallback {
     @Override
     public void onSuccess(@Nullable LoginResult loginResult) {
         bridge.triggerWindowJSEvent("mfKYCLoginSuccess", String.format("{ 'login success': %s }", loginResult.getIdentityId()));
+    }
+
+
+    public static class MatiLoginButtonLauncher extends AppCompatActivity
+    {
+        public static WeakReference<MatiCallbackManager> weakReferenceCallbackManager;
+        Handler mMainHandler ;
+        MatiLoginButton mMatiLoginButton;
+        @Override
+        protected void onCreate(@Nullable Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(getView());
+            mMainHandler = new Handler(this.getMainLooper());
+            mMainHandler.post (new Runnable() {
+                @Override
+                public void run() {
+                    mMatiLoginButton.performClick();
+                }
+            });
+
+        }
+
+        public View getView(){
+            FrameLayout layout = new FrameLayout(this);
+            layout.setLayoutParams(new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT,
+                    FrameLayout.LayoutParams.MATCH_PARENT));
+            mMatiLoginButton = new MatiLoginButton(this);
+            mMatiLoginButton.setVisibility(View.INVISIBLE);
+            FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(100, 100);
+            params.setMargins(-100, 0, 0, 0);
+            mMatiLoginButton.setLayoutParams(params);
+            layout.addView(mMatiLoginButton);
+
+            return layout;
+        }
+
+        @Override
+        protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+            super.onActivityResult(requestCode, resultCode, data);
+            if(weakReferenceCallbackManager != null && weakReferenceCallbackManager.get() != null){
+                if(weakReferenceCallbackManager.get().onActivityResult(requestCode, resultCode, data)){
+                    this.finish();
+                }
+            }
+
+        }
     }
 }
 
