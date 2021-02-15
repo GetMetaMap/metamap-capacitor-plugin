2e520fdedc02f0ac8acdbd6417250d9f20c200d8
andrey.posnov <andrey.posnov@mati.io>
2021-02-15 13:51:21 +0300

    fix npm package names

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,6 +1,6 @@
 declare module '@capacitor/core' {
   interface PluginRegistry {
-    AposnovmatiMatiCapacitorPlugin: MatiCapacitorPluginPlugin;
+    MatiCapacitorPlugin: MatiCapacitorPluginPlugin;
   }
 }
 
