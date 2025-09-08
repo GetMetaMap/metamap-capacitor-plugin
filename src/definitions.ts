87318b892b7a191b975b46bcaaf81bbdb5abcad7
Sergio Vallejo <vallejosergio@gmail.com>
2025-09-08 11:56:42 -0300

    feat: Added addListener method

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,9 +1,12 @@
+import { type PluginListenerHandle } from "@capacitor/core";
+
 export interface MetaMapParams {
   clientId: string
   flowId: string
   metadata?: object
 }
 export interface MetaMapCapacitorPlugin {
+  addListener(eventName: 'verificationCreated', listenerFunc: (data: { identityId: string, verificationID: string }) => any): Promise<PluginListenerHandle>;
   showMetaMapFlow(options: MetaMapParams): Promise<{ identityId: string, verificationID: string }>;
 }
 
